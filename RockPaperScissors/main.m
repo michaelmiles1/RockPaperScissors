9efbb59c1d993ef5af76cdf411d1c2e4322e98e6
Michael Miles <michael.gage.miles@gmail.com>
2019-08-13 19:07:18 -0700

    Initial Commit

---
+++ RockPaperScissors/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RockPaperScissors
+//
+//  Created by Michael Miles on 8/13/19.
+//  Copyright © 2019 Michael Miles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
