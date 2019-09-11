47ac308869cfb5fab7cf3770bb0854079277dbea
Michael Miles <michael.gage.miles@gmail.com>
2019-09-10 18:41:15 -0700

    Added code to run the project in the main.m file.

--- RockPaperScissors/main.m
+++ RockPaperScissors/main.m
@@ -7,11 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "RPSController.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        //Initialize RPSController
+        RPSController *gameController = [[RPSController alloc] init];
+        
+        [gameController throwDown:Rock];
     }
     return 0;
 }
